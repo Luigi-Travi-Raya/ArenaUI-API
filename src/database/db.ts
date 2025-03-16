a7c9a0c85b4b72740a82f06036b235ad2a5691ad
Luigi-Travi-Raya <traviraya@gmail.com>
2025-03-16 20:43:34 -0300

    feat: authentication of db on app start

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,7 +2,17 @@ import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize("arenaui", "root", "",{
     host: 'localhost',
-    dialect: "mysql"
+    dialect: "mysql",
+    logging: false
 });
+
+export const DatabaseAuthenticate = async () => {
+    try {
+        await sequelize.authenticate()
+        console.log("DB Connected!")
+    } catch (error) {
+        console.log("DB Auth Failed: " + error)
+    }
+}
 
 export default sequelize
